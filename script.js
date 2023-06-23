fc557447be629212c7f21bb4c4ad3aeef69a82a9
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-24 00:28:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+	 function updateOutput() {
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve("Hello, world!");
+          }, 1000);
+        });
+      }
+
+      function setText(elementId, text) {
+        const element = document.getElementById(elementId);
+        if (element) {
+          element.textContent = text;
+        }
+      }
+
+      // Call the updateOutput function and update the element text after the promise resolves
+      updateOutput().then((result) => {
+        setText("output", result);
+      });
